8e683255264b3f27e25f8eebfa19b78216276f3e
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-11-06 17:25:03 -0600

    DoITPDF

---
+++ DoITPDFController.js
@@ -0,0 +1,12 @@
+({
+   openModel: function(component, event, helper) {
+
+      component.set("v.isOpen", true);
+   },
+ 
+   closeModel: function(component, event, helper) {
+
+      component.set("v.isOpen", false);
+   },
+ 
+})
